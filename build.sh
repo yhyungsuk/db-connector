6757e4bcfb3bf28bf3c11e0cfb913866c8cf4f91
yhyungsuk <yhyungsuk@vmware.com>
2023-07-13 14:32:24 +0900

    first commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY> <DB_HOST> <DB_USER> <DB_PASSWORD>"
+    exit 1
+fi
+
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+DB_HOST=$3
+DB_USER=$4
+DB_PASSWORD=$5
+
+docker build -t db-connector .
+
+docker run -p 3001:3001 -d -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e DB_HOST=${DB_HOST} -e DB_USER=${DB_USER} -e DB_PASSWORD=${DB_PASSWORD} --name connector db-connector
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+const { Client } = require('pg');
+
+const app = express();
+
+app.get('/describe', (req, res) => {
+  describe()
+  res.send('describe');
+});
+
+app.get('/query', async (req, res) => {
+  await query()
+  res.send('query');
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
+
+
+AWS.config.update({
+  region: 'ap-northeast-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const describe = () => {
+  const rds = new AWS.RDS({ apiVersion: '2014-10-31' });
+
+  const params = {};
+
+  rds.describeDBInstances(params, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.DBInstances);
+    }
+  });
+}
+
+const query = async () => {
+  try {
+    const client = new Client({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'expo',
+      port: 5432,
+    })
+    client.connect()
+    console.log(222)
+
+    const result = await client.query('SELECT * FROM cards')
+    console.log(111)
+    console.log(result)
+
+    client.end()
+  } catch (e) {
+    console.error(e)
+  }
+}
